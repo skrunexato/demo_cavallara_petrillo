b8a32cc6c61d0dae5eb231465faa9160f6daff70
Kevin Cavallara <kcavallara@logicadomain.it>
2021-09-17 12:51:42 +0200

    first commit

---
+++ demo/src/test/java/com/demo_cavallara_petrillo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo_cavallara_petrillo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
