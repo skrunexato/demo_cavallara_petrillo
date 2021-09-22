6854ce8c1cd259bb23018d93d6bfdcbfdd827f65
kcavallara <kcavallara@PORTAT104.logicadomain.it>
2021-09-22 15:23:45 +0200

    controller azienda

--- demo/src/main/java/com/demo_cavallara_petrillo/controller/GestioneAzienda.java
+++ demo/src/main/java/com/demo_cavallara_petrillo/controller/GestioneAzienda.java
@@ -1,13 +1,18 @@
 package com.demo_cavallara_petrillo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo_cavallara_petrillo.dto.AziendaDto;
+import com.demo_cavallara_petrillo.model.Azienda;
+import com.demo_cavallara_petrillo.model.Dipendente;
 import com.demo_cavallara_petrillo.services.AppService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/azienda")
@@ -15,6 +20,12 @@ public class GestioneAzienda {
 
 	@Autowired
 	AppService service;
+	
+	@GetMapping("/listaAziende")
+	public List<Azienda> listAzienda (){
+		List<Azienda> listeAziende = service.getAllAziende();
+		return listeAziende;
+	}
 	
 	@PostMapping("")
 	public AziendaDto insertAzienda(@RequestBody AziendaDto dtoA) {
