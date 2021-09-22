46ff4e11113eae9b971cb83cc32474e8d64efa3d
kcavallara <kcavallara@PORTAT104.logicadomain.it>
2021-09-22 12:48:24 +0200

    model azienda

--- demo/src/main/java/com/demo_cavallara_petrillo/model/Azienda.java
+++ demo/src/main/java/com/demo_cavallara_petrillo/model/Azienda.java
@@ -16,7 +16,7 @@ public class Azienda {
 
 	@Id
 	@Column(name="IdAzienda")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;	
 	
 	@Column (name="Nome")
