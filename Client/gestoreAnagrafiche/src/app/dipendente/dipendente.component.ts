9351a3f6b226ed6ddd5d7846176e569ac4b50239
gianmarcopetrillo26 <gianmarcopetrillo26@gmail.com>
2021-09-29 12:00:08 +0200

    Nuovo component

---
+++ Client/gestoreAnagrafiche/src/app/dipendente/dipendente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dipendente',
+  templateUrl: './dipendente.component.html',
+  styleUrls: ['./dipendente.component.css']
+})
+export class DipendenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
