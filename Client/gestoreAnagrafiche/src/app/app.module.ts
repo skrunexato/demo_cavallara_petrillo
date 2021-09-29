197d25568f5fb07ada514da183269f45efce818b
skrunexato <kevincavallara@gmail.com>
2021-09-29 12:02:43 +0200

    kevin
    aggiunto component azienda

--- Client/gestoreAnagrafiche/src/app/app.module.ts
+++ Client/gestoreAnagrafiche/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DipendenteComponent } from './dipendente/dipendente.component';
+import { AziendaComponent } from './azienda/azienda.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DipendenteComponent
+    DipendenteComponent,
+    AziendaComponent
   ],
   imports: [
     BrowserModule,
---
+++ Client/gestoreAnagrafiche/src/app/azienda/azienda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-azienda',
+  templateUrl: './azienda.component.html',
+  styleUrls: ['./azienda.component.css']
+})
+export class AziendaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
